08ee1f4e3a71773c6462d74b0cf7b49635648123
Lihua <404467859@qq.com>
2018-08-05 15:06:28 +0800

    订单查询

--- dolaing-admin/src/main/java/com/dolaing/modular/mall/vo/OrderGoodsVo.java
+++ dolaing-admin/src/main/java/com/dolaing/modular/mall/vo/OrderGoodsVo.java
@@ -15,16 +15,15 @@ import java.math.BigDecimal;
  */
 @Data
 public class OrderGoodsVo {
-    /**
-     * 订单商品信息id
-     */
-    private Integer id;
+
+    private Integer id ;
+
     /**
      * 订单号
      */
     private Integer orderId;
     /**
-     * 商品的的id
+     * 订单商品信息id
      */
     private Integer goodsId;
     /**
@@ -43,6 +42,17 @@ public class OrderGoodsVo {
      * 商品的售价
      */
     private BigDecimal goodsPrice;
+    /**
+     * 品牌id
+     */
+    private String brandId ;
+
+    /**
+     * 品牌名
+     */
+    private String brandName ;
+
+
 
     /**
      * 土地编号
@@ -56,17 +66,39 @@ public class OrderGoodsVo {
      * 预计发货时间
      */
     private String expectDeliverTime ;
-    /**
-     * 预计单位产量
-     */
-    private String expectPartOutput ;
-    /**
-     * 商品总金额
-     */
-    private String goodsAmount ;
+
     /**
      * 定金比例
      */
-    private String depositRatio ;
+    private BigDecimal depositRatio ;
+    private BigDecimal landPartArea;//每单位面积
+    private String landPartAreaUnit;//每单位面积单位(默认亩)
+    private BigDecimal expectPartOutput;//预计单位产量
+    private String expectPartOutputUnit;//预计单位产量单位(默认KG)
+    private Integer isFreeShipping;//是否包邮(0不包邮 1包邮)
+    private String goodsMasterImgs;//产品主图([xx]号分隔)
+    //需计算值
 
+    private BigDecimal goodsAmount ;  //商品总金额
+    private String goodsMasterImg;//产品主图第一张
+    private BigDecimal buyLandArea;//认购面积
+
+
+    public BigDecimal getBuyLandArea() {
+        this.buyLandArea = this.landPartArea.multiply(new BigDecimal(this.goodsNumber));
+        return buyLandArea;
+    }
+
+
+    public BigDecimal getGoodsAmount() {
+        this.goodsAmount = this.goodsPrice.multiply(new BigDecimal(this.goodsNumber));
+        return goodsAmount;
+    }
+
+    public String getGoodsMasterImg() {
+        if(this.goodsMasterImgs !=null ){
+            this.goodsMasterImg = this.goodsMasterImgs.split(",")[0];
+        }
+        return goodsMasterImg;
+    }
 }
